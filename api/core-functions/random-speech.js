62852d549742c1082c791accaae0524b8e03d82f
Sea-Moryak <164082172+sergeyb2024@users.noreply.github.com>
2024-07-01 11:56:40 -0400

    Update random string generator

--- api/core-functions/random-speech.js
+++ api/core-functions/random-speech.js
@@ -6,7 +6,6 @@ const forNewPartName = ['New part: ', 'New part name:', 'The updated component i
 const forGeoDifference = ['Main purpose compared to latest revision: ', 'Major differences compared to previous versions: ', 'Key aspects that were taken into consideration: ', 'Noticable differences: ', 'The objective to choose this specific version over previous component: '];
 const forPrimaryReason = ['One of the many reasons for this specific update: ', 'The idea is to achieve a ', 'The expected improvement in the area of ', 'Engineers mainly focusing on the specifics linked to ', 'Principal reasons to bring in this particular update ', 'This specific update aims to improve '];
 const forDescritpions = ['Finally, a brief description: ', 'More details: ', 'Team further explains: ', 'Additional comments provided: ', 'More clues on how this update will work: ', 'A few more thoughts diving deeper into the nature of this update: '];
-
 /*
 will include the absolute minimum and maximum values
 when calculating radomly
@@ -22,5 +21,9 @@ const getRandomProperty = (arr) => {
 };
 
 module.exports = {
-  getRandomProperty, forNewPartName, forGeoDifference, forPrimaryReason, forDescritpions,
-};
+  getRandomProperty,
+  forNewPartName,
+  forGeoDifference,
+  forPrimaryReason,
+  forDescritpions,
+}
